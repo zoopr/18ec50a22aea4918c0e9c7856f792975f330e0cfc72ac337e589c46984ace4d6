7cf6968268d7b145eae4bb01bfdc329629860c58
zoopr <32989402+zoopr@users.noreply.github.com>
2018-02-01 18:56:27 +0100

    day 3. Comments and formatting fixes

---
+++ cmake-build-debug/CMakeFiles/cluedo.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "C:/Users/Mat/CLionProjects/cluedo/AI.c" "C:/Users/Mat/CLionProjects/cluedo/cmake-build-debug/CMakeFiles/cluedo.dir/AI.c.obj"
+  "C:/Users/Mat/CLionProjects/cluedo/BoardManager.c" "C:/Users/Mat/CLionProjects/cluedo/cmake-build-debug/CMakeFiles/cluedo.dir/BoardManager.c.obj"
+  "C:/Users/Mat/CLionProjects/cluedo/Gameplay.c" "C:/Users/Mat/CLionProjects/cluedo/cmake-build-debug/CMakeFiles/cluedo.dir/Gameplay.c.obj"
+  "C:/Users/Mat/CLionProjects/cluedo/Pieces.c" "C:/Users/Mat/CLionProjects/cluedo/cmake-build-debug/CMakeFiles/cluedo.dir/Pieces.c.obj"
+  "C:/Users/Mat/CLionProjects/cluedo/Standards.c" "C:/Users/Mat/CLionProjects/cluedo/cmake-build-debug/CMakeFiles/cluedo.dir/Standards.c.obj"
+  "C:/Users/Mat/CLionProjects/cluedo/main.c" "C:/Users/Mat/CLionProjects/cluedo/cmake-build-debug/CMakeFiles/cluedo.dir/main.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/cluedo.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cluedo.dir/main.c.obj"
+  "CMakeFiles/cluedo.dir/BoardManager.c.obj"
+  "CMakeFiles/cluedo.dir/Pieces.c.obj"
+  "CMakeFiles/cluedo.dir/Standards.c.obj"
+  "CMakeFiles/cluedo.dir/Gameplay.c.obj"
+  "CMakeFiles/cluedo.dir/AI.c.obj"
+  "cluedo.pdb"
+  "cluedo.exe"
+  "cluedo.exe.manifest"
+  "libcluedo.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/cluedo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
